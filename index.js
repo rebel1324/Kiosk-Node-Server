16a928d80c466bd56bac432ee1831bfa51497be4
Kim HyunWoo <stories2@naver.com>
2018-06-02 22:33:51 +0900

    index module : make http request redirect to https

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
+const sslRedirect = require('heroku-ssl-redirect');
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app = express()
+app.use(sslRedirect())
+app.use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
